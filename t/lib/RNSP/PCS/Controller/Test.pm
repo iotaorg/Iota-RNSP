a6a6c100750894fa4b7db6d6b3966217408b1bf5
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2012-07-23 23:57:44 -0300

    mocks and libs for tests
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

---
+++ t/lib/RNSP/PCS/Controller/Test.pm
@@ -0,0 +1,16 @@
+
+package RNSP::PCS::Controller::Test;
+
+use namespace::autoclean;
+
+use Moose;
+BEGIN { extends 'Catalyst::Controller'; }
+
+__PACKAGE__->config( namespace => '' );
+
+sub auto : Private {
+    my ( $self, $c ) = @_;
+}
+
+1;
+
