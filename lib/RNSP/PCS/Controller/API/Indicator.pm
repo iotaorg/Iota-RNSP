c7a9e44d46b101b99d7297a3f0282bbb2edb9189
renato aware <renato@aware.com.br>
2012-08-20 04:33:59 -0300

    indicadores

---
+++ lib/RNSP/PCS/Controller/API/Indicator.pm
@@ -0,0 +1,243 @@
+
+package RNSP::PCS::Controller::API::Indicator;
+
+use Moose;
+use JSON qw(encode_json);
+
+BEGIN { extends 'Catalyst::Controller::REST' }
+
+__PACKAGE__->config( default => 'application/json' );
+
+sub base : Chained('/api/base') : PathPart('indicador') : CaptureArgs(0) {
+  my ( $self, $c ) = @_;
+  $c->stash->{collection} = $c->model('DB::Indicator');
+
+}
+
+sub object : Chained('base') : PathPart('') : CaptureArgs(1) {
+  my ( $self, $c, $id ) = @_;
+  $c->stash->{object} = $c->stash->{collection}->search_rs( { 'me.id' => $id } );
+  $c->stash->{object}->count > 0 or $c->detach('/error_404');
+}
+
+sub indicador : Chained('object') : PathPart('') : Args(0) : ActionClass('REST') {
+  my ( $self, $c ) = @_;
+
+}
+
+=pod
+
+=encoding utf-8
+
+detalhe da variavel
+
+GET /api/indicador/$id
+
+Retorna:
+
+    {
+        "created_at": "2012-08-20 03:24:39.379529",
+        "formula": "$a + $b",
+        "goal": "1",
+        "name": "xx",
+        "axis": "Y",
+        "url": "http://localhost/api/indicador/32",
+        "created_by": {
+            "name": "admin",
+            "id": 1
+        }
+    },
+
+=cut
+
+sub indicador_GET {
+  my ( $self, $c ) = @_;
+  my $object_ref  = $c->stash->{object}->search(undef, {prefetch => ['owner']})->as_hashref->next;
+
+  $self->status_ok(
+    $c,
+    entity => {
+      created_by => {
+        map { $_ => $object_ref->{owner}{$_} } qw(name id)
+      },
+      (map { $_ => $object_ref->{$_} } qw(name goal axis formula created_at))
+    }
+  );
+}
+
+=pod
+
+atualizar variavel
+
+POST /api/indicador/$id
+
+Retorna:
+
+    indicador.update.name         Texto: Nome
+    indicador.update.formula      Texto: formula das variaveis
+    indicador.update.goal         Texto: numero que se quer chegar
+    indicador.update.axis         Texto: (talvez seja a categoria)
+
+
+=cut
+
+sub indicador_POST {
+  my ( $self, $c ) = @_;
+    $self->status_forbidden( $c, message => "access denied", ), $c->detach
+    unless $c->check_user_roles(qw(admin));
+
+
+  $c->req->params->{indicador}{update}{id} = $c->stash->{object}->next->id;
+
+  my $dm = $c->model('DataManager');
+
+  $self->status_bad_request( $c, message => encode_json( $dm->errors ) ), $c->detach
+    unless $dm->success;
+
+  my $obj = $dm->get_outcome_for('indicador.update');
+
+  $self->status_accepted(
+    $c,
+    location =>
+      $c->uri_for( $self->action_for('indicador'), [ $obj->id ] )->as_string,
+        entity => { name => $obj->name, id => $obj->id }
+    ),
+    $c->detach
+    if $obj;
+}
+
+
+=pod
+
+apagar variavel
+
+DELETE /api/indicador/$id
+
+Retorna: No-content ou Gone
+
+=cut
+
+sub indicador_DELETE {
+  my ( $self, $c ) = @_;
+
+    $self->status_forbidden( $c, message => "access denied", ), $c->detach
+    unless $c->check_user_roles(qw(admin));
+
+
+  my $obj = $c->stash->{object}->next;
+  $self->status_gone( $c, message => 'deleted' ), $c->detach unless $obj;
+
+  $obj->delete;
+
+  $self->status_no_content($c);
+}
+
+sub list : Chained('base') : PathPart('') : Args(0) : ActionClass('REST') {
+}
+
+
+=pod
+
+listar variaveis
+
+GET /api/indicador
+
+Retorna:
+
+    {
+        "users": [
+            {
+                "created_at": "2012-08-20 03:24:39.379529",
+                "formula": "$a + $b",
+                "goal": "1",
+                "name": "BB",
+                "axis": "Y",
+                "url": "http://localhost/api/indicador/32",
+                "created_by": {
+                    "name": "admin",
+                    "id": 1
+                }
+            },
+            ...
+        ]
+    }
+
+=cut
+
+sub list_GET {
+  my ( $self, $c ) = @_;
+
+    my @list = $c->stash->{collection}->search_rs( undef, { prefetch => 'owner' } )->as_hashref->all;
+    my @objs;
+
+    foreach my $obj (@list){
+        push @objs, {
+
+
+            created_by => {
+                map { $_ => $obj->{owner}{$_} } qw(name id)
+            },
+
+            (map { $_ => $obj->{$_} } qw(name goal axis formula created_at)),
+            url => $c->uri_for_action( $self->action_for('indicador'), [ $obj->{id} ] )->as_string,
+
+        }
+    }
+    $self->status_ok(
+        $c,
+        entity => {
+        indicadors => \@objs
+        }
+    );
+}
+
+
+=pod
+
+criar variavel
+
+POST /api/indicador
+
+Param:
+
+    indicador.create.name        Texto, Requerido: Nome
+    indicador.create.formula     Texto, Requerido: formula das variaveis
+    indicador.create.goal        Texto, Requerido: numero que se quer chegar
+    indicador.create.axis        Texto, Requerido: (talvez seja a categoria)
+
+Retorna:
+
+    {"name":"Foo Bar","id":3}
+
+
+=cut
+
+sub list_POST {
+  my ( $self, $c ) = @_;
+  $self->status_forbidden( $c, message => "access denied", ), $c->detach
+    unless $c->check_user_roles(qw(admin));
+
+
+  $c->req->params->{indicador}{create}{user_id} = $c->user->id;
+
+  my $dm = $c->model('DataManager');
+
+  $self->status_bad_request( $c, message => encode_json( $dm->errors ) ), $c->detach
+    unless $dm->success;
+  my $object = $dm->get_outcome_for('indicador.create');
+
+  $self->status_created(
+    $c,
+    location => $c->uri_for( $self->action_for('indicador'), [ $object->id ] )->as_string,
+    entity => {
+      name => $object->name,
+      id   => $object->id,
+
+    }
+  );
+
+}
+
+with 'RNSP::PCS::TraitFor::Controller::Search';
+1;
+
---
+++ lib/RNSP/PCS/Schema/ResultSet/Indicator.pm
@@ -0,0 +1,70 @@
+
+package RNSP::PCS::Schema::ResultSet::Variable;
+
+use namespace::autoclean;
+
+use Moose;
+extends 'DBIx::Class::ResultSet';
+with 'RNSP::PCS::Role::Verification';
+with 'RNSP::PCS::Schema::Role::InflateAsHashRef';
+
+use Data::Verifier;
+
+
+sub _build_verifier_scope_name {'indicator'}
+
+sub verifiers_specs {
+    my $self = shift;
+    return {
+        create => Data::Verifier->new(
+            profile => {
+                name        => { required => 1, type => 'Str' },
+                formula     => { required => 1, type => 'Str' }, # check Math::Expression::Evaluator
+                goal        => { required => 1, type => 'Num' },
+                axis        => { required => 1, type => 'Str' },
+                user_id     => { required => 1, type => 'Int' },
+            },
+        ),
+
+        update => Data::Verifier->new(
+            profile => {
+                id          => { required => 1, type => 'Int' },
+                name        => { required => 0, type => 'Str' },
+                formula     => { required => 0, type => 'Str' },
+                goal        => { required => 0, type => 'Num' },
+                axis        => { required => 0, type => 'Str' },
+            },
+        ),
+
+
+
+    };
+}
+
+sub action_specs {
+    my $self = shift;
+    return {
+        create => sub {
+            my %values = shift->valid_values;
+
+            my $var = $self->create( \%values );
+
+            $var->discard_changes;
+            return $var;
+        },
+        update => sub {
+            my %values = shift->valid_values;
+
+            do { delete $values{$_} unless defined $values{$_}} for keys %values;
+            return unless keys %values;
+
+            my $var = $self->find( delete $values{id} )->update( \%values );
+            $var->discard_changes;
+            return $var;
+        },
+
+    };
+}
+
+1;
+
