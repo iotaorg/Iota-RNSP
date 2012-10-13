72ebae88449c57266bfacc5143fb8cc71e1926ed
renato <renato@aware.com.br>
2012-10-13 09:33:39 -0300

    fix str to num nas variaveis basicas, incluido cadastro dos eixos

--- script/deploy/after_schema.sql
+++ script/deploy/after_schema.sql
@@ -21,21 +21,37 @@ drop table IF EXISTS movimentos;
 
 SELECT pg_catalog.setval('variable_id_seq', 40, true);
 
-INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (19, 'pop_total', 'População total', 'pop_total', 1, '2012-10-01 16:50:42.857155', 'str', 'yearly', NULL, true);
-INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (20, 'pop_rural_urbana', 'População rural e urbana', 'pop_rural_urbana', 1, '2012-10-01 16:51:55.453327', 'str', 'yearly', NULL, true);
-INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (22, 'pop_faixa', 'Divisão da população total por faixa etária', 'pop_faixa', 1, '2012-10-01 16:52:20.626508', 'str', 'yearly', NULL, true);
-INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (23, 'pop_genero', 'Divisão da população total por gênero', 'pop_genero', 1, '2012-10-01 16:52:42.933181', 'str', 'yearly', NULL, true);
-INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (24, 'pop_raca', 'Divisão da população total por raça/etnia', 'pop_raca', 1, '2012-10-01 16:53:05.478149', 'str', 'yearly', NULL, true);
-INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (26, 'densidade_demo', 'Densidade demográfica - O número de pessoas por quilômetro quadrado', 'densidade_demo', 1, '2012-10-01 16:57:19.059432', 'str', 'yearly', NULL, true);
-INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (27, 'area_municipio', 'Área do Município', 'area_municipio', 1, '2012-10-01 16:58:44.813519', 'str', 'yearly', NULL, true);
-INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (28, 'expect_vida', 'Expectativa de Vida: Esperança de vida ao nascer', 'expect_vida', 1, '2012-10-01 16:58:54.33095', 'str', 'yearly', NULL, true);
-INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (30, 'idh_municipal', 'IDH Municipal', 'idh_municipal', 1, '2012-10-01 16:59:08.447301', 'str', 'yearly', NULL, true);
-INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (31, 'gini', 'Gini', 'gini', 1, '2012-10-01 17:00:11.909949', 'str', 'yearly', NULL, true);
-INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (35, 'pib', 'Produto Interno Bruto per capita', 'pib', 1, '2012-10-01 17:00:35.676173', 'str', 'yearly', NULL, true);
-INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (36, 'renda_capita', 'Renda per capita', 'renda_capita', 1, '2012-10-01 17:00:49.800921', 'str', 'yearly', NULL, true);
-INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (38, 'part_eleitorado', 'Participação do eleitorado nas últimas eleições', 'part_eleitorado', 1, '2012-10-01 17:01:02.250016', 'str', 'yearly', NULL, true);
-INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (39, 'total_func', 'Total de funcionários empregados no município', 'total_func', 1, '2012-10-01 17:01:12.462152', 'str', 'yearly', NULL, true);
-INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (40, 'orcamento_liq', 'Orçamento liquidado', 'orcamento_liq', 1, '2012-10-01 17:01:22.614466', 'str', 'yearly', NULL, true);
+INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (19, 'pop_total', 'População total', 'pop_total', 1, '2012-10-01 16:50:42.857155', 'num', 'yearly', NULL, true);
+INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (20, 'pop_rural_urbana', 'População rural e urbana', 'pop_rural_urbana', 1, '2012-10-01 16:51:55.453327', 'num', 'yearly', NULL, true);
+INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (22, 'pop_faixa', 'Divisão da população total por faixa etária', 'pop_faixa', 1, '2012-10-01 16:52:20.626508', 'num', 'yearly', NULL, true);
+INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (23, 'pop_genero', 'Divisão da população total por gênero', 'pop_genero', 1, '2012-10-01 16:52:42.933181', 'num', 'yearly', NULL, true);
+INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (24, 'pop_raca', 'Divisão da população total por raça/etnia', 'pop_raca', 1, '2012-10-01 16:53:05.478149', 'num', 'yearly', NULL, true);
+INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (26, 'densidade_demo', 'Densidade demográfica - O número de pessoas por quilômetro quadrado', 'densidade_demo', 1, '2012-10-01 16:57:19.059432', 'num', 'yearly', NULL, true);
+INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (27, 'area_municipio', 'Área do Município', 'area_municipio', 1, '2012-10-01 16:58:44.813519', 'num', 'yearly', NULL, true);
+INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (28, 'expect_vida', 'Expectativa de Vida: Esperança de vida ao nascer', 'expect_vida', 1, '2012-10-01 16:58:54.33095', 'num', 'yearly', NULL, true);
+INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (30, 'idh_municipal', 'IDH Municipal', 'idh_municipal', 1, '2012-10-01 16:59:08.447301', 'num', 'yearly', NULL, true);
+INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (31, 'gini', 'Gini', 'gini', 1, '2012-10-01 17:00:11.909949', 'num', 'yearly', NULL, true);
+INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (35, 'pib', 'Produto Interno Bruto per capita', 'pib', 1, '2012-10-01 17:00:35.676173', 'num', 'yearly', NULL, true);
+INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (36, 'renda_capita', 'Renda per capita', 'renda_capita', 1, '2012-10-01 17:00:49.800921', 'num', 'yearly', NULL, true);
+INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (38, 'part_eleitorado', 'Participação do eleitorado nas últimas eleições', 'part_eleitorado', 1, '2012-10-01 17:01:02.250016', 'num', 'yearly', NULL, true);
+INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (39, 'total_func', 'Total de funcionários empregados no município', 'total_func', 1, '2012-10-01 17:01:12.462152', 'num', 'yearly', NULL, true);
+INSERT INTO variable (id, name, explanation, cognomen, user_id, created_at, type, period, source, is_basic) VALUES (40, 'orcamento_liq', 'Orçamento liquidado', 'orcamento_liq', 1, '2012-10-01 17:01:22.614466', 'num', 'yearly', NULL, true);
+
+insert into axis (id, name) values (1, 'Governança');
+insert into axis (id, name) values (2, 'Bens Naturais Comuns');
+insert into axis (id, name) values (3, 'Equidade, Justiça Social e Cultura de Paz');
+insert into axis (id, name) values (4, 'Gestão Local para a Sustentabilidade');
+insert into axis (id, name) values (5, 'Planejamento e Desenho Urbano');
+insert into axis (id, name) values (6, 'Cultura para a sustentabilidade');
+insert into axis (id, name) values (7, 'Educação para a Sustentabilidade e Qualidade de Vida');
+insert into axis (id, name) values (8, 'Economia Local, Dinâmica, Criativa e Sustentável');
+insert into axis (id, name) values (9, 'Consumo Responsável e Opções de Estilo de Vida');
+insert into axis (id, name) values (10, 'Melhor Mobilidade, Menos Tráfego');
+insert into axis (id, name) values (11, 'Ação Local para a Saúde');
+insert into axis (id, name) values (12, 'Do Local para o Global');
+insert into axis (id, name) values (13, 'Planejando Cidades do Futuro');
+SELECT setval('public.axis_id_seq', 100, true);
+
 
 create view movimentos as
 select
