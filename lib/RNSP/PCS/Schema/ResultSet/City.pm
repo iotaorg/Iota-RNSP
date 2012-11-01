14101bd733c593fa4367f438723cdad9e5e8fdb6
renato <renato@aware.com.br>
2012-11-01 12:06:09 -0200

    fix city-2 update

--- lib/RNSP/PCS/Schema/ResultSet/City.pm
+++ lib/RNSP/PCS/Schema/ResultSet/City.pm
@@ -85,10 +85,11 @@ sub action_specs {
             my $name_o     = $values{name};
 
             my $idx = 2;
-            while (defined $self->search( {
+            while (my $item = defined $self->search( {
                 uf       => $values{uf},
                 name_uri => $values{name_uri},
             })->next){
+                next if ($item->id == $values{id});
                 $values{name_uri} = $name_uri_o . '-'. $idx;
                 $values{name}     = $name_o     . '-'. $idx++;
             };
