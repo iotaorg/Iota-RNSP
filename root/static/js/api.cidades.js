5e702fec0e59bc44e684c7248c2bc4a48b0d3451
Henry Shinohara <shin@aware.com.br>
2012-11-01 23:32:40 -0200

    reajusta tela de acordo com "tamanho" do perfil da cidade

--- root/static/js/api.cidades.js
+++ root/static/js/api.cidades.js
@@ -62,10 +62,19 @@ function showCidadeData(){
 		));
 	});
 
-	setMap(cidade_data.cidade.latitude,cidade_data.cidade.longitude);
-
 	$("#cidades-dados .image").css("background-image","none");
 	$("#cidades-dados .image").css("background-image","url('/static/images/"+cidade_data.cidade.imagem+"')");
+	
+	var diff = $("#cidades-dados .profile .content-fill").height() - $("#cidades-dados .profile").height() + 10;
+	if (diff > 10){
+		$("#cidades-dados .profile").css("height","+="+diff);
+		$("#cidades-dados .resume").css("height","+="+diff);
+		$("#cidades-dados .map").css("height","+="+diff);
+		$("#cidades-dados #mapa").css("height","+="+diff);
+	}
+
+	setMap(cidade_data.cidade.latitude,cidade_data.cidade.longitude);
+	
 }
 
 function loadIndicadoresData(){
