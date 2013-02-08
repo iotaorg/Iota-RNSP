976ef21ea2ef684e54e170bfdff9f6b0f7425769
Renato Cron <renato@aware.com.br>
2013-02-08 11:48:15 -0200

    Gerenciamento de fontes

---
+++ lib/RNSP/PCS/Schema/ResultSet/Source.pm
@@ -0,0 +1,70 @@
+
+package RNSP::PCS::Schema::ResultSet::Source;
+
+use namespace::autoclean;
+
+use Moose;
+use Text2URI;
+my $text2uri = Text2URI->new();    # tem lazy la, don't worry
+
+extends 'DBIx::Class::ResultSet';
+with 'RNSP::PCS::Role::Verification';
+with 'RNSP::PCS::Schema::Role::InflateAsHashRef';
+
+use Data::Verifier;
+use JSON qw /encode_json/;
+
+sub _build_verifier_scope_name { 'source' }
+
+sub verifiers_specs {
+    my $self = shift;
+    return {
+        create => Data::Verifier->new(
+            profile => {
+                name                        => { required => 1, type => 'Str' },
+                user_id                     => { required => 1, type => 'Int' },
+            },
+        ),
+
+        update => Data::Verifier->new(
+            profile => {
+                id                          => { required => 1, type => 'Int' },
+                name                        => { required => 1, type => 'Str' },
+            },
+        ),
+
+    };
+}
+
+sub action_specs {
+    my $self = shift;
+    return {
+        create => sub {
+            my %values = shift->valid_values;
+
+            do { delete $values{$_} unless defined $values{$_} }
+              for keys %values;
+            return unless keys %values;
+
+            my $var = $self->create( \%values );
+
+            $var->discard_changes;
+            return $var;
+        },
+        update => sub {
+            my %values = shift->valid_values;
+
+            do { delete $values{$_} unless defined $values{$_} }
+              for keys %values;
+            return unless keys %values;
+
+            my $var = $self->find( delete $values{id} )->update( \%values );
+            $var->discard_changes;
+            return $var;
+        },
+
+    };
+}
+
+1;
+
