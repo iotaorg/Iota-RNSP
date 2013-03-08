733242e1b09d8ed3a7512e5ea708d5a08f579a8b
Renato Cron <renato@aware.com.br>
2013-03-08 10:59:13 -0300

    configuracao do db

--- dbic_dump.sh
+++ dbic_dump.sh
@@ -1,4 +1,4 @@
-perl script/iota_create.pl model DB DBIC::Schema Iota::Schema create=static components=TimeStamp,PassphraseColumn 'dbi:Pg:dbname=iota_pcs;host=localhost' postgres system quote_names=1 overwrite_modifications=1
+perl script/iota_create.pl model DB DBIC::Schema Iota::Schema create=static components=TimeStamp,PassphraseColumn 'dbi:Pg:dbname=iota;host=localhost' postgres system quote_names=1 overwrite_modifications=1
 
 rm lib/Iota/Model/DB.pm.new;
 
