314d3bc3ecec47791324c22ed93b361eb0ed4d17
Renato Cron <renato@aware.com.br>
2013-04-16 18:27:12 -0300

    debuging

--- lib/Iota.pm
+++ lib/Iota.pm
@@ -95,6 +95,7 @@ around 'apply_default_middlewares' => sub {
 
     sub {
         my $env = shift;
+        use DDP; p $env;
         if ( $env->{ PATH_INFO } =~ m/^\/(prefeitura|movimento)(.*)/i ) {
 
             my $sites = {
