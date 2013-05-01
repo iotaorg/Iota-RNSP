8a029df7c2c2ff6bdaff7bbaeb3b793a135cb9af
Renato Cron <renato@aware.com.br>
2013-04-30 21:12:27 -0300

    scripts para migrar o banco da v3 sem cache para a com cache

---
+++ script/atualiza_valores_indicadores.pl
@@ -0,0 +1,24 @@
+use strict;
+use warnings;
+use utf8;
+
+use FindBin qw($Bin);
+use lib "$Bin/../lib";
+
+use Config::General;
+use Template;
+
+use Encode;
+use JSON qw / decode_json /;
+
+
+use Iota;
+my $schema = Iota->model('DB');
+
+use Iota::IndicatorData;
+
+my $data = Iota::IndicatorData->new(
+    schema  => $schema
+);
+
+$data->upsert();
