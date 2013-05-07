d592256b61531a463df80c1a30af739754a6942f
Renato Cron <renato@aware.com.br>
2013-05-07 18:21:31 -0300

    pages where fic

--- lib/Iota/Controller/API/Page.pm
+++ lib/Iota/Controller/API/Page.pm
@@ -13,6 +13,12 @@ sub base : Chained('/api/base') : PathPart('page') : CaptureArgs(0) {
     $c->stash->{collection} = $c->model('DB::UserPage');
 
 
+    if ($c->check_any_user_role(qw(user))) {
+        $c->stash->{collection} = $c->stash->{collection}->search({
+            'me.user_id' => $c->user->id
+        })
+    }
+
 }
 
 sub object : Chained('base') : PathPart('') : CaptureArgs(1) {
