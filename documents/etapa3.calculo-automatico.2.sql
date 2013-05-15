47eb825bafd3cd60e07c02d46c35548d931164d1
Renato Cron <renato@aware.com.br>
2013-05-15 15:41:52 -0300

    SQL com as colunas a mais para diferenciar os dados ativos dos dados calculados sozinhos

---
+++ documents/etapa3.calculo-automatico.2.sql
@@ -0,0 +1,6 @@
+-- campos para indicadar que um dado foi salvo automaticamente e
+-- qual dado deve ser usado
+alter table indicator_value add column active_value boolean not null default true;
+alter table indicator_value add column generated_by_compute boolean not null default false;
+create index ix_indicator_value__active_value on indicator_value(active_value);
+
