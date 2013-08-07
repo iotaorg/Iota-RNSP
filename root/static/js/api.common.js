1f9b92e109d134753a453f6dad10c23a148d23a5
Henry Shinohara <shin@aware.com.br>
2013-08-07 11:18:14 -0300

    array.indexOf fix para IE8

--- root/static/js/api.common.js
+++ root/static/js/api.common.js
@@ -19,6 +19,25 @@ if (!String.prototype.render) {
 	};
 }
 
+if (!Array.prototype.indexOf){
+  Array.prototype.indexOf = function(elt /*, from*/){
+    var len = this.length >>> 0;
+
+    var from = Number(arguments[1]) || 0;
+    from = (from < 0)
+         ? Math.ceil(from)
+         : Math.floor(from);
+    if (from < 0)
+      from += len;
+
+    for (; from < len; from++){
+      if (from in this &&
+          this[from] === elt)
+        return from;
+    }
+    return -1;
+  };
+}
 
 var _on_func = function (e) {
     var $elm = $(e.target);
