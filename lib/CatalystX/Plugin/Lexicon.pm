6913e49c245362dcdfe2711171f3a49fdf63e60f
Renato Cron <renato@aware.com.br>
2013-08-26 16:31:55 -0300

    ?????

--- lib/CatalystX/Plugin/Lexicon.pm
+++ lib/CatalystX/Plugin/Lexicon.pm
@@ -91,10 +91,7 @@ sub loc {
         my $user_id = $c->user ? $c->user->id : $c->config->{admin_langs_id};
         my @add_langs = split /,/, $c->config->{forced_langs};
 
-        my $enfu = '@@';
         foreach my $lang (@add_langs){
-
-        $enfu.="$lang+";
             my $str = $lang eq $default ? $text : "? $text";
             $cache->{$lang}{$text} = $str;
 
@@ -105,10 +102,8 @@ sub loc {
                 lex_value => $str,
                 user_id   => $user_id
             });
-
         }
-
-        return $current_lang eq $default ? "$enfu$text" : "! $current_lang/$default: $enfu$text";
+        return $current_lang eq $default ? $text : "? $text";
     }
 
 }
