fb168501117d4a38321502dc1254790dabe34de9
Renato Cron <renato@aware.com.br>
2013-11-25 19:01:26 -0200

    new scrool

--- root/static/v2/js/utils.js
+++ root/static/v2/js/utils.js
@@ -15,3 +15,32 @@ $.fn.disableSelection = function () {
         .on('selectstart', false);
 };
 
+$('html').on('click', 'a[data-animated-anchor]', function(){
+    var me=$(this), hash = me.attr('href'), id = hash.replace('#', ''),
+    node = $(hash), offset_y = parseInt(node.attr('data-animated-anchor-offset'), 10) || 0;
+
+    node.removeAttr('id');
+
+    window.location.hash = hash;
+
+    node.attr('id', id);
+
+    $('html, body').animate({
+        scrollTop: node.offset().top + offset_y
+    }, 600, function(){
+
+        for(i=0;i<1;i++) {
+            node.fadeTo(500, 0.5).fadeTo(500, 1.0);
+        }
+
+    });
+    return false;
+});
+
+$(function(){
+    var hash = window.location.hash, who = hash ? $('a[href="'+hash+'"]') : null;
+
+    if (who){
+        who.click();
+    }
+});
