5edf2658d32e769afc31d61dab4ad3b49fba48a2
Renato Cron <renato@aware.com.br>
2013-11-25 19:16:22 -0200

    fix number of fires during onload

--- root/static/v2/js/utils.js
+++ root/static/v2/js/utils.js
@@ -17,29 +17,21 @@ $.fn.disableSelection = function () {
 
 $('html').on('click', 'a[data-animated-anchor]', function(){
     var me=$(this), hash = me.attr('href'), id = hash.replace('#', ''),
-    node = $(hash), offset_y = parseInt(node.attr('data-animated-anchor-offset'), 10) || 0;
+    node = $('#id_' + id), offset_y = parseInt(node.attr('data-animated-anchor-offset'), 10) || 0;
 
-    node.removeAttr('id');
-
-    window.location.hash = hash;
-
-    node.attr('id', id);
-
+    window.location.hash = '#' + id;
     $('html, body').animate({
         scrollTop: node.offset().top + offset_y
     }, 600, function(){
-
         for(i=0;i<1;i++) {
             node.fadeTo(500, 0.5).fadeTo(500, 1.0);
         }
-
     });
     return false;
 });
 
-$(function(){
-    var hash = window.location.hash, who = hash ? $('a[href="'+hash+'"]') : null;
-
+$(window).load(function(){
+    var hash = window.location.hash, who = hash ? $('a[href="'+hash+'"]:first') : null;
     if (who){
         who.click();
     }
