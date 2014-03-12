100d234453f6fda9959ba35f369719da882b43ee
Renato Cron <renato@aware.com.br>
2014-03-12 14:49:07 -0300

    script atualizar senha

---
+++ script/atualiza_senha_usuarios.pl
@@ -0,0 +1,43 @@
+use strict;
+use warnings;
+use utf8;
+
+use FindBin qw($Bin);
+use lib "$Bin/../lib";
+
+use Config::General;
+use Template;
+
+use Encode;
+use JSON qw / decode_json /;
+
+use Iota;
+my $schema = Iota->model('DB');
+
+
+my @not_sent = ();
+
+
+open (my $f, '<:utf8', $ARGV[0]) or die 'cant find <' . $ARGV[0] . $!;
+
+while (my $l = <$f>){
+    $l =~ /^\s*([^;]+);([^\s]+)\s*$/;
+    push @not_sent, {email => $1, password => $2};
+}
+
+use DDP;
+my $x = \@not_sent;
+p $x;
+print "Confirm? say yes! \n";
+$x = <STDIN>;
+die ('not accepted') unless $x =~ /yes/;
+
+foreach my $mail (@not_sent) {
+
+    my $who = $schema->resultset('User')->search( {
+        email => $mail->{email}
+    } );
+
+    print $who->update( { password => $mail->{password}} ), "\n";
+
+}
