30d728c90cc0124ba678d618d6468fc84aca597e
Renato Cron <renato@aware.com.br>
2014-11-14 12:20:11 -0200

    tratando encoding bugado

--- lib/Iota/Controller/RDF/Variable.pm
+++ lib/Iota/Controller/RDF/Variable.pm
@@ -21,6 +21,7 @@ sub object : Chained('base') PathPart('') CaptureArgs(1) {
     $c->stash->{object} = $c->stash->{collection}->search_rs( { 'me.id' => $id } )->next;
     $c->stash->{object} or $c->detach('/error_404_rdf');
 }
+use Encode qw(encode decode);
 
 sub show : Chained('object') PathPart('') Args(0) {
     my ( $self, $c, $id ) = @_;
@@ -40,18 +41,36 @@ sub show : Chained('object') PathPart('') Args(0) {
 
     # reserva memoria uma vez só
     my %str = ();
+#use Devel::Peek qw(Dump);
 
     # name => dct:title
     %str = $c->valid_values_for_lex_key( $object->name );
+#use DDP; p \%str;
+#Dump $str{'pt-br'};
 
+#$str{$_} = encode ('UTF-8', $str{$_}) for keys %str;
     $rdf->assert_literal( $uri, 'dct:title', $rdf->new_literal( $str{$_} , $_ )) for keys %str;
 
     # explanation => dct:description
     %str = $c->valid_values_for_lex_key( $object->explanation );
-    $rdf->assert_literal( $uri, 'dct:description', $rdf->new_literal( $str{$_} , $_ )) for keys %str;
+#$str{$_} = encode ('UTF-8', $str{$_}) for keys %str;
 
-    $c->res->body($rdf->serialize( format => $c->stash->{serialize_formart} ));
-    $c->res->header('Content-Type', $c->stash->{format_vs_contenttype}{$c->stash->{serialize_formart}});
+#use DDP; p $rdf->model->as_hashref;
+    #use Data::Dumper; use DDP; p Dumper $rdf;
+    #use DDP; p $rdf->serialize(format => 'rdfjson');
+    #use JSON::XS;
+    #use DDP; p encode_json ($rdf->model->as_hashref);
+
+    $rdf->assert_literal( $uri, 'dct:description', $rdf->new_literal( $str{$_}, $_ )) for keys %str;
+
+    my $x = $rdf->serialize( format => $c->stash->{serialize_format} );
+    if ($c->stash->{serialize_format} eq 'rdfxml'){
+        use DDP; p $x;
+    }
+    $c->res->body($x);
+    #$c->res->body(encode_json($rdf->model->as_hashref));
+    $c->res->header('Content-Type', $c->stash->{format_vs_contenttype}{$c->stash->{serialize_format}});
+    #$c->res->content_type('application/json; charset=utf-8');
 }
 
 __PACKAGE__->meta->make_immutable;
