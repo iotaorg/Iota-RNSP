befd6dec2606f505b67db033495eb7a0a8ec7704
RenatoCRON <renato.cron@gmail.com>
2015-05-29 11:11:25 -0300

    oxe

--- lib/Iota/IndicatorData.pm
+++ lib/Iota/IndicatorData.pm
@@ -8,6 +8,7 @@ has schema => (
     isa      => 'Any',
     required => 1
 );
+use Encode qw/encode/;
 
 our $DEBUG = 0;
 
@@ -769,7 +770,7 @@ sub _get_indicator_values {
                             my $valor = $formula->evaluate_with_alias(@calcvars);
 
                             $out->{$region_id}{$user_id}{ $indicator->id }{$date}{$variation} =
-                              [ $valor, [ keys %sources ], encode_json( {@calcvars} ), [ keys %observations ] ];
+                              [ $valor, [ keys %sources ], encode('UTF-8', encode_json( {@calcvars} )), [ keys %observations ] ];
                         }
 
                     }
@@ -778,7 +779,7 @@ sub _get_indicator_values {
 
                         # '' = variacao
                         $out->{$region_id}{$user_id}{ $indicator->id }{$date}{''} =
-                          [ $valor, [ keys %sources ], encode_json( \%values ), [ keys %observations ] ];
+                          [ $valor, [ keys %sources ], encode('UTF-8', encode_json( \%values )), [ keys %observations ] ];
                     }
 
                 }
