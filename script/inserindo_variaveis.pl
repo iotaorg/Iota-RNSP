d397dadd029ce359f778e22cfcb359cce6f6ff81
Renan Carvalho <renan.azevedo.carvalho@gmail.com>
2015-08-07 15:52:45 -0300

    script para importar csv
    Signed-off-by: Renan Carvalho <renan.azevedo.carvalho@gmail.com>

---
+++ script/inserindo_variaveis.pl
@@ -0,0 +1,34 @@
+use strict;
+use warnings;
+use utf8;
+use Parse::CSV;
+use DDP;
+use FindBin qw($Bin);
+use lib "$Bin/../lib";
+
+use Config::General;
+use Template;
+
+use Encode;
+use JSON qw / decode_json /;
+
+use Iota;
+my $schema = Iota->model('DB');
+
+open my $fh, '<', 'documents/variaveis-premio-crianca.csv' or die 'not open';
+
+my $objects = Parse::CSV->new(
+    handle   => $fh,
+    sep_char => ';',
+    names    => [
+        qw/name explanation cognomen user_id type period source is_basic measurement_unit_id user_type summarization_method/
+    ]
+);
+while ( my $object = $objects->fetch ) {
+    $object->{measurement_unit_id} = undef;
+    $object->{source}              = undef;
+
+    $schema->resultset('Variable')->create( { %{$object} } )
+      unless $schema->resultset('Variable')
+      ->search( { cognomen => $object->{cognomen} } )->next;
+}
