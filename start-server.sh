ce824e39b9292aba51b546d8fc41b3bdf405a502
renato cron <renato.cron@gmail.com>
2017-06-22 05:20:27 -0300

    Use start_server program on start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -3,4 +3,4 @@
 # feito para ser usado apenas dentro do container docker
 cd /src;
 source /home/app/perl5/perlbrew/etc/bashrc
-CATALYST_CONFIG=/src/iota.conf starman --port 8080 --workers 4 --error-log /data/log/starman.log --user app --group app iota.psgi 
+KILL_OLD_DELAY=5 CATALYST_CONFIG=/src/iota.conf start_server --port 8080 -- starman --workers 4 --error-log /data/log/starman.log --user app --group app iota.psgi 
