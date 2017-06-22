3766268f77b00cba4fcd80457fbc0952389a1937
renato cron <renato.cron@gmail.com>
2017-06-22 06:12:02 -0300

    Comentarios uteis em start-server.sh

--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# feito para ser usado apenas dentro do container docker
+# script feito para ser usado apenas dentro do container docker
+
+# OBS: só da para mudar workers de maneira persistente matando o start_server com INT, 
+# logo, o restart deixa de ser graceful, entao escolha com cuidado
+# Atualmente iota usa aprox 200mb ~ 350mb de Memoria RAM por worker
+export WORKERS=4
+
 cd /src;
 source /home/app/perl5/perlbrew/etc/bashrc
 mkdir -p /data/envdir
@@ -11,4 +17,8 @@ CATALYST_CONFIG=/src/iota.conf start_server \
  --signal-on-hup=QUIT \
  --kill-old-delay=10 \
  --port=8080 \
- -- starman --workers 4 --error-log /data/log/starman.log -MCatalyst -MDBIx::Class --user app --group app iota.psgi 
+ -- starman \
+ 	--workers $WORKERS \
+ 	--error-log /data/log/starman.log \
+ 	-MCatalyst -MDBIx::Class -MJSON::XS -MMoose \
+ 	--user app --group app iota.psgi 
