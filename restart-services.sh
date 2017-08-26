de773e4926c17c560a943b0ec3857966009878f8
Renato Cron <renato.cron@gmail.com>
2017-08-26 15:09:49 -0300

    fix restart-services.sh

--- restart-services.sh
+++ restart-services.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# API 
+# API
 export PIDFILE=/tmp/start_server.pid
 if [ -e "$PIDFILE" ]; then
 	kill -HUP $(cat $PIDFILE)
 fi
+
+cd /src;
+source /home/app/perl5/perlbrew/etc/bashrc
+
+sqitch deploy -t local
 
 # email daemon
 # ps aux|grep -v grep| grep script/emails_queue_sender.pl| awk '{print $2}' | xargs kill
