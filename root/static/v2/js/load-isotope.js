07ec05f2ce20411e31f419c3b0e54826ea2d6b7b
Renato Cron <renato.cron@gmail.com>
2017-09-28 07:56:36 -0300

    Add new class of isotope

--- root/static/v2/js/load-isotope.js
+++ root/static/v2/js/load-isotope.js
@@ -12,15 +12,25 @@ var $recalc_isotope = function(){
             masonry: { columnWidth: $container.width() / 4 }
         });
     });
+
+
+    $('.isotope-container').each(function(i, o){
+        var $container = $(o);
+        $container.isotope({});
+    });
 };
 
 $(document).ready(function() {
 
-    $('.isotope-container2,.isotope-container4').isotope({
+    $('.isotope-container2,.isotope-container4,.isotope-container').isotope({
         itemSelector: '.element',
         resizable: false,
     });
     $recalc_isotope();
 });
+
+setTimeout($recalc_isotope, 1000);
+setTimeout($recalc_isotope, 5000);
+setTimeout($recalc_isotope, 15000);
 
 $(window).smartresize($recalc_isotope);
